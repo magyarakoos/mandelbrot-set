cbdb99dd606c41f8d009e7372725be6749836bea
magyarakoos <ninjaoffical08@gmail.com>
2024-02-11 19:03:51 +0100

    arch descktop sync: 2024-02-11 19:03:51

--- src/mandelbrot.cpp
+++ src/mandelbrot.cpp
@@ -7,5 +7,9 @@ int Mand(const Complex& c) {
     int it = 0;
     Complex z {0, 0};
 
-    while (it < ITER && abs(z.real) )
+    while (it < ITER && abs(z.real) <= 2 && abs(z.i) <= 2) {
+
+        z *= z;
+        z += c;
+    }
 }
