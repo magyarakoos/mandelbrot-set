1d9bcdc6f0b6191affdc90bef3467422df1a7265
magyarakoos <ninjaoffical08@gmail.com>
2024-02-11 20:12:56 +0100

    arch descktop sync: 2024-02-11 20:12:56

--- src/main.cpp
+++ src/main.cpp
@@ -115,7 +115,7 @@ int main() {
     }
 
     while (!WindowShouldClose()) {
-        std::cerr << real_min << ' ' << real_max << ' ' << img_min << ' ' << img_max << ' ' << tint << '\n';
+        //std::cerr << real_min << ' ' << real_max << ' ' << img_min << ' ' << img_max << ' ' << tint << '\n';
         Tick();
     }
 }
