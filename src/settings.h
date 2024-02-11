80a4e776e3b05edb57e5bb6b8b6736e56b148aff
magyarakoos <ninjaoffical08@gmail.com>
2024-02-11 19:32:18 +0100

    arch descktop sync: 2024-02-11 19:32:18

--- src/settings.h
+++ src/settings.h
@@ -1,15 +1,15 @@
 
 inline constexpr int
-    FPS = 10,
+    FPS = 60,
     WIDTH = 1000,
     HEIGHT = 1000,
     ITER = 100
 ;
 
 inline constexpr long double
-    REAL_MIN = -0.1,
-    REAL_MAX = 0.1,
-    IMG_MIN = -0.1,
+    REAL_MIN = -2,
+    REAL_MAX = 2,
+    IMG_MIN = -2,
     IMG_MAX = 0.1
 ;
 
