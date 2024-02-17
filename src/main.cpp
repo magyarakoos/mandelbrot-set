cb0004cb405139b9ee29345137dc8779f8b561f6
LordOfPumpkins <ninjaoffical08@gmail.com>
2024-02-17 14:33:59 +0100

    arch descktop sync: 2024-02-17 14:33:59

--- src/main.cpp
+++ src/main.cpp
@@ -4,6 +4,40 @@
 #include <assert.h>
 #include <array>
 
+// #include <boost/multiprecision/cpp_dec_float.hpp>
+// using BigFloat = boost::multiprecision::cpp_dec_float_50;
+
+inline constexpr int
+    FPS = 60,
+    ITER = 200
+;
+
+inline constexpr double
+    WIDTH = 800,
+    HEIGHT = 800,
+    REAL_MIN = -2,
+    REAL_MAX = 2,
+    IMG_MIN = -2,
+    IMG_MAX = 2,
+    ZOOM_STEP = 0.05
+;
+
+inline constexpr float
+    // pink: 0.76f
+    // light blue: 0.5f
+    // green: 0.32f
+    // yellow: 0.14f
+    // orange:0.04f
+    // red: 0.0f
+
+    TINT = 0.64f,
+    TINT_STEP = 0.02f
+;
+
+inline constexpr char
+    TITLE[] = "Mandelbrot Set"
+;
+
 #include "mandelbrot.h"
 #include "settings.h"
 
@@ -20,8 +54,7 @@ double
 ;
 
 float 
-    tint = TINT,
-    zoom_level = 1
+    tint = TINT
 ;
 
 // yes, I am terrified of the C raw array
