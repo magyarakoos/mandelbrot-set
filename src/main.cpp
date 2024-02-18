06491979f3990a240ea3237c23f61471fae4035b
LordOfPumpkins <ninjaoffical08@gmail.com>
2024-02-18 09:58:44 +0100

    arch descktop sync: 2024-02-18 09:58:44

--- src/main.cpp
+++ src/main.cpp
@@ -84,10 +84,10 @@ void Tick(bool update) {
     }
 
     std::string s = 
-        Stringify(tint,     2) + " " + 
-        Stringify(real_min, 2) + " " +
-        Stringify(real_max, 2) + " " +
-        Stringify(img_min,  2) + " " +
+        Stringify(tint,     2) + "  " + 
+        Stringify(real_min, 2) + "  " +
+        Stringify(real_max, 2) + "  " +
+        Stringify(img_min,  2) + "  " +
         Stringify(img_max,  2)
     ;
 
